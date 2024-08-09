7a29312993dc13db7991bfdc58cb7405d9c1c7f6
Jaska-ITishnik <jasurbekbekmirzayev2004@gmail.com>
2024-08-09 18:51:03 +0500

    example.py qo'shildi

--- example.py
+++ example.py
@@ -1,41 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import shutil
 import requests
-# sudo apt update
-# sudo apt install -y chromium-chromedriver
 
-# PATH variable - ChromeDriver path
-PATH = "/usr/bin/chromedriver"
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(PATH, options=chrome_options)
 
-service = Service(PATH)
-# Define the driver with options
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-# URL to the Instagram reel
 url = 'https://www.instagram.com/reel/url/'
+# Open the YouTube video URL in the browser
 driver.get(url)
 
-# Find the specific script tag containing the video URL
 find = driver.find_element(by=By.XPATH, value='/html/head/script[3]')
 text = find.get_attribute('innerText')
 driver.close()
 
-# Extract the video URL from the script content
 start = text.index('"contentUrl":"')
 end = text.index('","thumbnailUrl"', start + 14)
+print(text)
 new_text = text[start + 14:end].replace('\/', '/')
 
-# Print the extracted URL for verification
 print('--------------')
 print(new_text)
-
-# Download the video from the extracted URL
 response = requests.get(new_text, stream=True)
 with open('output.mp4', 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
